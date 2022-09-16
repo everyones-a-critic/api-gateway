566e63d1027cc8ec4c47a2df9769ebd81d74eebc
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 09:39:11 -0500

    Adds outputs for lambda service

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "gateway_id" {
+  value       = aws_api_gateway_rest_api.main.id
+}
+
+output "root_resource_id" {
+  value       = aws_api_gateway_rest_api.main.root_resource_id
+}
+
+output "authorizer_id" {
+    value = aws_api_gateway_authorizer.main.id
+}
